c9a9f7e28a0265a940b0fbc60b26dc4103df9820
Claudineijoseprill <32801503+Claudineijoseprill@users.noreply.github.com>
2023-02-18 12:01:23 -0300

    Create ejemplo.sh

---
+++ ejemplo.sh
@@ -0,0 +1,20 @@
+
+
+from api_mk import ApiRos 
+
+ser = ApiRos("10.0.5.67", "play", "poi")
+if not ser.loged:
+    sys.exit(0)
+
+
+ser.command = '/interface/wireless/registration-table/getall'
+ser.send()
+if ser.error == '':
+    print('OK', len(ser.datos), ser.status)
+
+    for i in range(len(ser.datos)):
+        if not 'mac-address' in ser.datos[i].keys():
+            print(ser.datos[i])
+        else: print(ser.datos[i]['radio-name'])
+        
+else: print('ERROR', ser.error)
